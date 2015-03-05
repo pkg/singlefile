19510f231c4e21c0c73f5825a5b8efad0f0e3572
Dave Cheney <dave@cheney.net>
2015-03-05 15:23:32 +1100

    add more tests

--- singlefile_test.go
+++ singlefile_test.go
@@ -12,3 +12,19 @@ func TestLock(t *testing.T) {
 		t.Fatalf("could not release lock: %v", err)
 	}
 }
+
+func TestLockTwice(t *testing.T) {
+	const key = "somekey"
+	unlock, err := Lock(key)
+	if err != nil {
+		t.Fatalf("could not lock key %q: %v", key, err)
+	}
+	u2, err := Lock(key)
+	if err == nil {
+		u2()
+		t.Fatalf("second Lock should have failed")
+	}
+	if err := unlock(); err != nil {
+		t.Fatalf("could not release lock: %v", err)
+	}
+}
